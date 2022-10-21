6eeb8111f574208a169af204967706d73acee1e6
Olga Garbuzova <olgarbuzova84@.gmail.com>
2022-10-21 14:56:59 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-$(function(){
+$(function () {
 
-    let showPopup = function(){
+    let showPopup = function () {
         $('.popup-container').css('display', 'flex');
     };
 
-    let closePopup = function(){
+    let closePopup = function () {
         $('.popup-container').css('display', 'none');
     };
 
-    let clearForm = function(){
+    let clearForm = function () {
         $('.popup-container > input[type="text"]').val('');
     };
 
-    $('.order-call').on('click', function(){
-        showPopup();
-    });
-
-    $('.close').on('click', function(){
+    $('.close').on('click', function () {
         clearForm();
         closePopup();
     });
